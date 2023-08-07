51c68c17daa3e14dde52548d397719ddb66560db
lolainlondon <134396043+lolainlondon@users.noreply.github.com>
2023-08-07 18:50:57 +0100

    added id for each Tracklist

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,27 +1,27 @@
 export const playlist = [
     {
-     
+        id: "vcdse",
         trackImage: 'https://',
         trackTitle: "Bubbles",
         trackAlbum: "Hits 96",
         trackArtist: "Some person"
     },
     {
-     
+        id: "aacee",
         trackImage: 'https://',
         trackTitle: "Dana nana",
         trackAlbum: "Hits 96",
         trackArtist: "Jude the obscure"
     },
     {
-     
+        id: "ajkle",
         trackImage: 'https://',
         trackTitle: "Party",
         trackAlbum: "Hits 96",
         trackArtist: "People"
     },
     {
-      
+        id: "dcsse",
         trackImage: 'https://',
         trackTitle: "Love",
         trackAlbum: "Hits 96",
--- src/Components/Tracks/Tracklist.js
+++ src/Components/Tracks/Tracklist.js
@@ -1,27 +1,27 @@
 export const tracklist = [
     {
-       
+        id: "abcde",
         trackImage: 'https://',
         trackTitle: "The way you are",
         trackAlbum: "Hits 96",
         trackArtist: "Bob the cat"
     },
     {
-    
+        id: "abdse",
         trackImage: 'https://',
         trackTitle: "Ripley Biscuits",
         trackAlbum: "Hits 96",
         trackArtist: "Ned and the atomic dustbin"
     },
     {
-       
+        id: "aedde",
         trackImage: 'https://',
         trackTitle: "Making it up as we go along",
         trackAlbum: "Hits 96",
         trackArtist: "Alan"
     },
     {
-     
+        id: "ailde",
         trackImage: 'https://',
         trackTitle: "You know nothing Jon Snow",
         trackAlbum: "Hits 96",
